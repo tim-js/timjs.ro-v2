5cb01a35fe7769ddbe761ab56bfdad1b84f3eacd
ionescu-rares <62030412+ionescu-rares@users.noreply.github.com>
2024-03-09 11:04:03 +0200

    Fix home page redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://timjs.netlify.app",
   integrations: [tailwind(), mdx(), sitemap()],
   redirects: { "/": "/home" },
 });
