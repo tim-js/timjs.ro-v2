74aca6e6435f2e4d6cac09d184add18629783230
ionescu-rares <62030412+ionescu-rares@users.noreply.github.com>
2024-05-26 16:46:40 +0300

    Update font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
+        sans: [
+          "Reddit Sans",
+          "Inter Variable",
+          "Inter",
+          ...defaultTheme.fontFamily.sans,
+        ],
       },
       colors: {
         brand: "#EFC418",
