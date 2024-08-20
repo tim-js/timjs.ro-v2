d0c5bb7d9de8051426cd792bf8bbb6d06c3568ff
Adrian Faciu <adrian.faciu@hey.com>
2024-08-20 22:24:13 +0300

    Remove unused files

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,22 +2,6 @@
 import { z, defineCollection } from 'astro:content';
 
 // 2. Define your collection(s)
-const blogCollection = defineCollection({
-  schema: z.object({
-    draft: z.boolean(),
-    title: z.string(),
-    snippet: z.string(),
-    image: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
-    publishDate: z.string().transform(str => new Date(str)),
-    author: z.string().default('timjscommunity'),
-    category: z.string(),
-    tags: z.array(z.string()),
-  }),
-});
-
 const teamCollection = defineCollection({
   schema: ({ image }) =>
     z.object({
@@ -35,6 +19,5 @@ const teamCollection = defineCollection({
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  'blog': blogCollection,
   'team': teamCollection,
 };
