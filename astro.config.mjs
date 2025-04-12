51b027b4fea05fa8a2a9f1a21ed305fb435cc237
ionescu-rares <62030412+ionescu-rares@users.noreply.github.com>
2025-04-12 12:34:13 +0300

    Upgrade astro-icon and add bluesky

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.timjs.ro",
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
   trailingSlash: "always",
   redirects: {
     "/meetup-feedback/": {
